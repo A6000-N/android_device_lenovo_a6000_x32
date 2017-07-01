0bd52d742d6fdcbb0383544124e21916abed466c
Hassan Sardar <hassansardar08@live.com>
2017-07-01 20:55:56 +0000

    Fix Toolchains Path
    Signed-off-by: Hassan Sardar <hassansardar08@live.com>

--- board/kernel.mk
+++ board/kernel.mk
@@ -5,5 +5,5 @@ TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
 TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
 TARGET_KERNEL_SOURCE := kernel/lenovo/a6000
 TARGET_KERNEL_CONFIG := lineageos-wt86518_defconfig
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/arm-eabi-4.8/bin
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.8/bin
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-eabi-
