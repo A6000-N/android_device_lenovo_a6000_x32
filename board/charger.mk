9e5abe7f5bed54a304dbdcdeeb2f4a4b1a3b6b70
Hassan Sardar <hassansardar08@live.com>
2017-07-04 19:05:23 +0000

    a6000: Enable Real Time Lockscreen Charging Info
    Signed-off-by: Hassan Sardar <hassansardar08@live.com>

--- board/charger.mk
+++ board/charger.mk
@@ -2,3 +2,6 @@
 BOARD_CHARGER_ENABLE_SUSPEND := true
 BOARD_CHARGER_DISABLE_INIT_BLANK := true
 BACKLIGHT_PATH := /sys/class/leds/lcd-backlight/brightness
+
+# Enable real time lockscreen charging current values
+BOARD_GLOBAL_CFLAGS += -DBATTERY_REAL_INFO
