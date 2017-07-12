65f3432a0598a3c07fefe94d4b9bd0bcf6391966
Hassan Sardar <hassansardar08@live.com>
2017-07-12 20:00:24 +0000

    DND Bringup!
    Change-Id: Ifc1e847018a6ee94b2c296ed5bc0ea9ea81a6d66
    Signed-off-by: Hassan Sardar <hassansardar08@live.com>

--- dnd.mk
+++ dnd.mk
@@ -14,8 +14,8 @@
 
 $(call inherit-product, device/lenovo/a6000/full_a6000.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common DarkNess stuff.
+$(call inherit-product, vendor/dnd/config/common_full_phone.mk)
 
 # Must define platform variant before including any common things
 TARGET_BOARD_PLATFORM_VARIANT := msm8916
@@ -23,7 +23,7 @@ TARGET_BOARD_PLATFORM_VARIANT := msm8916
 # Assert
 TARGET_OTA_ASSERT_DEVICE := Kraft-T,a6000,K30-T,A6000,Kraft-W,Kraft-C,k30t,msm8916,Kraft-A6000,wt86518
 
-PRODUCT_NAME := lineage_a6000
+PRODUCT_NAME := dnd_a6000
 BOARD_VENDOR := Lenovo
 
 # Build fingerprint
@@ -32,3 +32,9 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="Kraft-A6000-user 5.0.2 LRX22G Kraft-A6000_S061_160727 release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-lenovo
+
+# Magisk
+WITH_ROOT=true
+
+# AudioFX
+WITH_AUDIOFX=true
